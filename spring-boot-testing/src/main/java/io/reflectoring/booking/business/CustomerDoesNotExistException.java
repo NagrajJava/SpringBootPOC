9ebbcc58fd2b4b77e5225a38948e6b48aae1b255
Nagraj <nagraj.507165@gmail.com>
2020-08-04 16:10:12 +0530

    Added log in CustomerDoesNotExistException

--- spring-boot-testing/src/main/java/io/reflectoring/booking/business/CustomerDoesNotExistException.java
+++ spring-boot-testing/src/main/java/io/reflectoring/booking/business/CustomerDoesNotExistException.java
@@ -1,9 +1,14 @@
 package io.reflectoring.booking.business;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class CustomerDoesNotExistException extends RuntimeException {
+	Logger log = LoggerFactory.getLogger(this.getClass());
 
   CustomerDoesNotExistException(Long customerId) {
     super(String.format("A customer with ID '%d' doesn't exist!", customerId));
+    log.info("Inside CustomerDoesNotExistException constructor....");
   }
 
 }
